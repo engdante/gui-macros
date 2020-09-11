8b345fc113b101434daf2e03a552f8ec8817bafa
engdante <eng.daniel.dimitrov@gmail.com>
2020-09-11 22:12:25 +0300

    Signed-off-by: engdante <eng.daniel.dimitrov@gmail.com>

---
+++ config.py
@@ -0,0 +1,24 @@
+appMacros = {
+    # 'caps lock' : 'macMacros_togle',
+    't': 'macros.fallback',
+    'm': 'macros.macMove',
+    'c': 'macros.macCopy',
+    'g': 'macros.macGrid_view_togle',
+    'h': 'macros.macGrid_onoff_togle'
+   }
+
+appSettings = {
+    "gui_update_ms" : "500",
+    'threshold': '0.95',
+    'disable_macros': '0',
+    'disable_macros_text_0' : 'Running',
+    'disable_macros_text_1' : 'Suspend',
+    'disable_macros_color_0' : 'blue',
+    'disable_macros_color_1' : 'red',
+    'disable_gridSnap' : '0',
+    'disable_gridSnap_color_0' : 'green',
+    'disable_gridSnap_color_1' : 'red',
+    'disable_gridSnap_text_0' : 'Grid ON',
+    'disable_gridSnap_text_1' : 'Grid OFF',
+    'last_key' : '_'
+}
