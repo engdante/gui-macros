4d991ba429508d4a4df4f6af9857c13453d727eb
engdante <eng.daniel.dimitrov@gmail.com>
2020-09-11 22:10:10 +0300

    Signed-off-by: engdante <eng.daniel.dimitrov@gmail.com>

--- macros.py
+++ macros.py
@@ -3,31 +3,13 @@ import cv2
 import os
 import sys
 import time
-import pyautogui
+import keyboard
+import mouse
 from PIL import ImageGrab
-from configparser import ConfigParser
-from pynput import keyboard
-from pynput.keyboard import Key, Controller
-from pynput.keyboard import Listener as KeyboardListener
-
-keyCont = Controller()
-
+import config
 
 scrPath = os.path.dirname(os.path.realpath(sys.argv[0]))
 imgPath = scrPath + "\Image"
-
-def get_config():
-    conf_file = ConfigParser()
-    conf_file.read("macros.ini")
-    return conf_file
-
-def set_config(level1, level2, value):
-    # print (level1, level2, value)
-    conf_file = ConfigParser()
-    conf_file.read("macros.ini")
-    conf_file.set(level1, level2, value)
-    with open('./macros.ini', 'w') as f:
-        conf_file.write(f)
 
 def get_screen():
     img = ImageGrab.grab(bbox = None)
@@ -43,9 +25,7 @@ def find_pic_in_screen(pic_name):
     img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
     res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
-    conf_file = get_config()
-    threshold = conf_file.getfloat("settings", "threshold")
-    loc = np.where( res >= threshold)
+    loc = np.where( res >= float(config.appSettings["threshold"]))
     #print (loc)
     if loc[0].size == 0:
         print ("{} is not find!".format(pic_name))
@@ -57,87 +37,68 @@ def fallback():
     print ("fallback is running")
     return
 
-def capslock_state():
-    import ctypes
-    hllDll = ctypes.WinDLL ("User32.dll")
-    VK_CAPITAL = 0x14
-    return hllDll.GetKeyState(VK_CAPITAL)
-
-def macMacros_togle():
-    print ("macMacros_togle is running")
-    # capslock = capslock_state()
-    
-    # if capslock == 65409:
-    #     set_config("settings", "disable_macros","1")
-    #     conf_file = get_config()
-    #     # print ('disable_macros is {}'.format(conf_file.getboolean("settings", "disable_macros")))
-    # else:
-    #     set_config("settings", "disable_macros","0")
-    #     conf_file = get_config()
-    #     # print ('disable_macros is {}'.format(conf_file.getboolean("settings", "disable_macros")))
-    return
-    
 def macGrid_view_togle():
-    current_xy = pyautogui.position()
+    current_xy = mouse.get_position()
     print ("macGrid_view_togle is running")
     
     icon_name = "view_menu.png"
     pic_xy = find_pic_in_screen(icon_name)
     try:
-        pyautogui.click(pic_xy[0],pic_xy[1])
+        mouse.move(pic_xy[0],pic_xy[1],absolute=True)
+        mouse.click()
     except:
         pass
     time.sleep(0.25)
     icon_name = "show_grid_menu.png"
     pic_xy = find_pic_in_screen(icon_name)
     try:
-        pyautogui.click(pic_xy[0],pic_xy[1])
+        mouse.move(pic_xy[0],pic_xy[1],absolute=True)
+        mouse.click()
     except:
         pass
     time.sleep(0.25)
 
-    pyautogui.moveTo(current_xy)
+    mouse.move(current_xy[0],current_xy[1],absolute=True)
     return
 
 def macGrid_onoff_togle():
-    current_xy = pyautogui.position()
+    current_xy = mouse.get_position()
     print ("macGrid_onoff_togle is running")
 
     icon_name = "view_menu.png"
     pic_xy = find_pic_in_screen(icon_name)
     try:
-        pyautogui.click(pic_xy[0],pic_xy[1])
+        mouse.move(pic_xy[0],pic_xy[1],absolute=True)
+        mouse.click()
     except:
         pass
     time.sleep(0.25)
     icon_name = "snap_grid_menu.png"
     pic_xy = find_pic_in_screen(icon_name)
     try:
-        pyautogui.click(pic_xy[0],pic_xy[1])
+        mouse.move(pic_xy[0],pic_xy[1],absolute=True)
+        mouse.click()
     except:
         pass
     time.sleep(0.25)
 
-    pyautogui.moveTo(current_xy)
+    mouse.move(current_xy[0],current_xy[1],absolute=True)
 
-    conf_file = get_config()
-    status_snap = conf_file.getboolean("settings", "disable_gridsnap")
-    if status_snap:
-        set_config("settings", "disable_gridSnap","0")
+    if config.appSettings["disable_gridSnap"] == "1":
+        config.appSettings["disable_gridSnap"] = "0"
     else:
-        set_config("settings", "disable_gridSnap","1")
-    conf_file = get_config()
-
+        config.appSettings["disable_gridSnap"] = "1"
     return
 
 def macMove():
     print ("macMove is running")
-    current_xy = pyautogui.position()
+    current_xy = mouse.get_position()
 
     icon_name = "move.png"
     pic_xy = find_pic_in_screen(icon_name)
     try:
-        pyautogui.click(pic_xy[0],pic_xy[1])
+        mouse.move(pic_xy[0],pic_xy[1],absolute=True)
+        mouse.click()
     except:
         return
     time.sleep(0.25)
@@ -145,22 +106,24 @@ def macMove():
     icon_name = "copy_active.png"
     pic_xy = find_pic_in_screen(icon_name)
     try:
-        pyautogui.click(pic_xy[0],pic_xy[1])
+        mouse.move(pic_xy[0],pic_xy[1],absolute=True)
+        mouse.click()
     except:
         pass
     time.sleep(0.25)
 
-    pyautogui.moveTo(current_xy)
+    mouse.move(current_xy[0],current_xy[1],absolute=True)
     return
 
 def macCopy():
     print ("macCopy is running")
-    current_xy = pyautogui.position()
+    current_xy = mouse.get_position()
 
     icon_name = "move.png"
     pic_xy = find_pic_in_screen(icon_name)
     try:
-        pyautogui.click(pic_xy[0],pic_xy[1])
+        mouse.move(pic_xy[0],pic_xy[1],absolute=True)
+        mouse.click()
     except:
         return
     time.sleep(0.25)
@@ -168,11 +131,11 @@ def macCopy():
     icon_name = "move_active.png"
     pic_xy = find_pic_in_screen(icon_name)
     try:
-        pyautogui.click(pic_xy[0],pic_xy[1])
+        mouse.move(pic_xy[0],pic_xy[1],absolute=True)
+        mouse.click()
     except:
         pass
     time.sleep(0.25)
 
-    pyautogui.moveTo(current_xy)
+    mouse.move(current_xy[0],current_xy[1],absolute=True)
     return
-
--- mainApp.py
+++ mainApp.py
@@ -1,17 +1,11 @@
 import os
 import sys
 import time
-from configparser import ConfigParser
-# from pynput.keyboard import Key, Controller
-import ctypes
-from pynput.keyboard import Listener as KeyboardListener
+import keyboard
 from tkinter import *
 from tkinter import messagebox
-# import macros.py file
-import macros as mac
-
-os.system("python create_config_file.py")
-# keyCont = Controller()
+import macros as macros
+import config
 
 class Application(Frame):
         """GUI to hold widgets. """
@@ -23,94 +17,46 @@ class Application(Frame):
         def createWidgets(self):
             """GUI widgets. """
             #Label instructing the user to take a guess
-            self.runningLabel = Label(self,text="Running", fg="blue", font=("Calibri", 14), padx=10)
+            self.runningLabel = Label(self,text="Running", fg="blue", font=("Calibri", 14), padx=15, width=6)
             self.runningLabel.grid(row=0,column=0)
-            self.gridLabel = Label(self,text="Grid ON", fg='green', font=("Calibri", 14), padx=10)
+            self.gridLabel = Label(self,text="Grid ON", fg='green', font=("Calibri", 14), padx=15, width=6)
             self.gridLabel.grid(row=0,column=1)
-            self.keyLabel = Label(self, text="_", fg='black', font=("Calibri", 14),relief="groove", padx=30)
+            self.keyLabel = Label(self, text="_", fg='black', font=("Calibri", 14),relief="groove", padx=30, width=2)
             self.keyLabel.grid(row=0,column=2)
-            self.helpLabel = Label(self, text="CapsLk - Disable Macros", fg='black', font=("Calibri", 10), justify="left", padx=10)
-            self.helpLabel.grid(row=0,column=3)
+            # self.helpLabel = Label(self, text="CapsLk - Disable Macros", fg='black', font=("Calibri", 10), justify="left", padx=10)
+            # self.helpLabel.grid(row=0,column=3)
 
         def update(self):
-            conf_file = get_config()
-            disable_macros = conf_file.getboolean("settings", "disable_macros")
-            disable_gridSnap = conf_file.getboolean("settings", "disable_gridSnap")
-            if disable_macros==True:
-                runningText = conf_file.get("settings", "disable_macros_text_1")
-                runningColor = conf_file.get("settings", "disable_macros_color_1")
+            disable_macros = config.appSettings["disable_macros"]
+            disable_gridSnap = config.appSettings["disable_gridSnap"]
+            if disable_macros=="1":
+                runningText = config.appSettings["disable_macros_text_1"]
+                runningColor = config.appSettings["disable_macros_color_1"]
             else:
-                runningText = conf_file.get("settings", "disable_macros_text_0")
-                runningColor = conf_file.get("settings", "disable_macros_color_0")
+                runningText = config.appSettings["disable_macros_text_0"]
+                runningColor = config.appSettings["disable_macros_color_0"]
             
-            if disable_gridSnap==True:
-                gridSnapText = conf_file.get("settings", "disable_gridSnap_text_1")
-                gridSnapColor = conf_file.get("settings", "disable_gridSnap_color_1")
+            if disable_gridSnap=="1":
+                gridSnapText = config.appSettings["disable_gridSnap_text_1"]
+                gridSnapColor = config.appSettings["disable_gridSnap_color_1"]
             else:
-                gridSnapText = conf_file.get("settings", "disable_gridSnap_text_0")
-                gridSnapColor = conf_file.get("settings", "disable_gridSnap_color_0")
+                gridSnapText = config.appSettings["disable_gridSnap_text_0"]
+                gridSnapColor = config.appSettings["disable_gridSnap_color_0"]
             
-            lastKey = conf_file.get("settings", "last_key")
+            lastKey = config.appSettings["last_key"]
 
             self.runningLabel.config(text=runningText, fg=runningColor)
             self.gridLabel.config(text=gridSnapText, fg=gridSnapColor)
             self.keyLabel.config(text=lastKey)
 
-def get_config():
-    conf_file = ConfigParser()
-    conf_file.read("macros.ini")
-    return conf_file
-
-def set_config(level1, level2, value):
-    # print (level1, level2, value)
-    conf_file = ConfigParser()
-    conf_file.read("macros.ini")
-    conf_file.set(level1, level2, value)
-    with open('./macros.ini', 'w') as f:
-        conf_file.write(f)
-
-def on_press(key):
-    key_string = str(key)
-    key_string = key_string.replace("\'", "")
-    key_string = key_string.replace("\"", "")
-    key_string = key_string.lower()
-
-    set_config("settings", "last_key",key_string)
-    conf_file = get_config()
-    disable_macros = conf_file.getboolean("settings", "disable_macros")
-
-    if disable_macros==True and key_string != 'key.caps_lock':
-        print ('Macros are DIASBLE !!!')
-        return
-    try:
-        call_macros = conf_file.get("macros", key_string)
-    except:
-        call_macros = "fallback"
-    call_macros = "mac." + call_macros + "()"
-    eval(call_macros)
-
-    app.update()
-    return   
-
-def win32_event_filter(msg, data):
-    if msg == 256 and data.vkCode == 20:
-        dll = ctypes.WinDLL('User32.dll')
-        # msdn.microsoft.com/en-us/library/dd375731
-        VK_CAPITAL = 0X14
-        if dll.GetKeyState(VK_CAPITAL) == 0:
-            dll.keybd_event(VK_CAPITAL, 0X3a, 0X1, 0)
-            time.sleep(0.01)
-            dll.keybd_event(VK_CAPITAL, 0X3a, 0X3, 0)
-            time.sleep(0.01)
-        
-        print("test")
-        
-    return
+            # print ("app update!!!")
+            self.after(int(config.appSettings["gui_update_ms"]), self.update)
 
 def runGui():
     root = Tk()
-    root.title("Macro GUI")
+    root.title("Macro GUI / press CapsLock to disable macros")
     root.geometry('425x30')
+    root.wm_attributes("-topmost", 1)
     root.protocol("WM_DELETE_WINDOW", on_closing)
     global app
     app = Application(root)
@@ -129,16 +75,53 @@ def exitApp():
     os._exit(0)
     return
 
+def capslock_state():
+    import ctypes
+    hllDll = ctypes.WinDLL ("User32.dll")
+    VK_CAPITAL = 0x14
+    cl_state = hllDll.GetKeyState(VK_CAPITAL) 
+    if cl_state == 1 or cl_state == 65409:
+        return True
+    return False
+
+def macros_togle():
+    print ("macros_togle is running")
+    capslock = capslock_state()
+       
+    if capslock:
+        config.appSettings["disable_macros"] = "1"
+        for key, value in config.appMacros.items():
+            try:
+                keyboard.remove_hotkey(key)
+            except:
+                print ("Error while removing {0} hotkey!".format(key))
+    else:
+        config.appSettings["disable_macros"] = "0"
+        for key, value in config.appMacros.items():
+            try:
+                keyboard.add_hotkey(key, eval(value), args=(), suppress=True, timeout=1, trigger_on_release=True)
+            except:
+                print ("Error while adding {0} hotkey!".format(key))
+    return
+
+def lastKey(key):
+    config.appSettings["last_key"] = key.name
+    return
+
 def mainApp():
     time.sleep(1)
-   
-    with KeyboardListener(on_press=on_press, win32_event_filter=win32_event_filter, suppress=True) as listener:
-        listener.join()
 
-    # pynput Package Documentation
-    # https://pynput.readthedocs.io/en/latest/index.html
+    macros_togle()
+    app.update()
+    keyboard.add_hotkey('caps lock', macros_togle, args=(), suppress=False, timeout=1, trigger_on_release=False)
+    keyboard.on_release(lastKey, suppress=False)
+
+    # keyboard Package Documentation
+    # https://github.com/boppreh/keyboard
     
 import concurrent.futures
 with concurrent.futures.ThreadPoolExecutor() as executor:
     thread1 = executor.submit(runGui)
     thread2 = executor.submit(mainApp)
+
+
